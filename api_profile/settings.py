afbc851adbf0860c2b9da98d863b4d4dab4bb31e
tayh <tayhcarvalho@gmail.com>
2018-09-27 20:14:33 -0400

    Change external ports

--- api_profile/settings.py
+++ api_profile/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'j&t8^37*%k9jjgb34lihevkkj)a39a*#8vod%%gclt-xv4ejt%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'user_profile',
 ]
 
