8559c4d66dc859ca3fec889514b530688e067831
lievertom <lievertom@gmail.com>
2018-10-04 22:58:03 -0300

    Delete CarProfile and integration with Integra id
    Co-authored-by: Lieverton Santos <lievertom@gmail.com>
    Co-authored-by: João Gabriel Antunes <jg.antunes.medeiros@gmail.com>

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,11 +1,6 @@
 from django.db import models
-# from django.contrib.postgres.fields import ArrayField, JSONField
-
-
-class CarProfile(models.Model):
-    notification_token = models.CharField(max_length=100)
-    plate = models.CharField(max_length=7)
 
 
 class Profile(models.Model):
     notification_token = models.CharField(max_length=100)
+    id_token = models.CharField(max_length=100)
