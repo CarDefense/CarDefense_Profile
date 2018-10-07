bc025ea23a47dac1bc74fb42d39396ef060af694
lievertom <lievertom@gmail.com>
2018-10-07 16:50:12 -0300

    Changing the token id type to integer

--- user_profile/models.py
+++ user_profile/models.py
@@ -3,4 +3,4 @@ from django.db import models
 
 class Profile(models.Model):
     notification_token = models.CharField(max_length=100)
-    id_token = models.CharField(max_length=100)
+    id_token = models.IntegerField()
